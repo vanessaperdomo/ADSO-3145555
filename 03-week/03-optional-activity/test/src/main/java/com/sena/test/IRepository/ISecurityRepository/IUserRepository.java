fa31673471614b7bf723fb7845ab5bdb45407397
vanessaperdomo <p.laurav2@gmail.com>
2026-03-03 02:20:58 -0500

    evidencia

--- 03-week/03-optional-activity/test/src/main/java/com/sena/test/IRepository/ISecurityRepository/IUserRepository.java
+++ 03-week/03-optional-activity/test/src/main/java/com/sena/test/IRepository/ISecurityRepository/IUserRepository.java
@@ -1,5 +1,16 @@
 package com.sena.test.IRepository.ISecurityRepository;
 
-public class IUserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+import com.sena.test.Entity.Security.Role;
+
+@Repository
+public interface IUserRepository extends JpaRepository<Role, Long> {
+
+    @Query("SELECT r FROM Role r WHERE r.roleName like %?1%")
+    List<Role> findByName(String roleName);
 
 }
